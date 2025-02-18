45ebc4fe1af436e8900f05ce065924ad57394afa
Gorosss <jongorostegui@gmail.com>
2025-02-18 21:41:54 +0100

    Css changes and fixes

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -13,19 +13,19 @@ import { BlogListComponent } from '../blog-list/blog-list.component';
 export class BlogComponent {
   blogList : IVideoGame[] = [
     {
-      "title": "Nuevo Zelda anunciado",
+      "title": "New Zelda",
       "img": "https://media.revistagq.com/photos/6105a5e0170e728db45e7649/16:9/w_1280,c_limit/Zelda-Breath-of-the-Wild.jpeg",
-      "text": "Nintendo ha anunciado un nuevo juego de Zelda...",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit",
       "publicationDate": "2025-02-15",
-      "gameReleaseDate": "2025-02-15",
+      "gameReleaseDate": "2026-05-15",
       "prize": 60
     },
     {
-      "title": "DLC de Elden Ring",
+      "title": "Elden Ring DLC Shadow of the Elden Lord",
       "img": "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/02/elden-ring-3277980.jpg",
-      "text": "FromSoftware lanzará un DLC para Elden Ring...",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit",
       "publicationDate": "2025-02-15",
-      "gameReleaseDate": "2024-02-15",
+      "gameReleaseDate": "2026-02-11",
       "prize": 60
     }
   ];
